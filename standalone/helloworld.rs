7081054230837538399930190d7893fc8989ca18
runaydhaygude <runaydhaygude97@gmail.com>
2026-01-01 22:10:02 +0530

    hello world

---
+++ standalone/helloworld.rs
@@ -0,0 +1,14 @@
+/*
+This is a simple Rust program that prints "Hello, world!" to the console.
+
+To run this program:
+    1. Make sure you have Rust installed. You can download it from https://www.rust-lang.org/tools/install.
+    2. Save this code in a file named `helloworld.rs`.
+    3. Open a terminal and navigate to the directory where you saved the file.
+    4. Compile the program using the command: `rustc helloworld.rs`.
+    5. Run the compiled program using the command: `./helloworld` on mac.
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
